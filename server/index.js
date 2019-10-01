28d61c13ec9a66ba865d0efc35a2e6f5d3324206
CristianPalta1 <cristian.palta.c@uniautonoma.edu.co>
2019-09-30 22:11:37 -0500

    commit Prod 4

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use((req,res,next) => {
   next();
 });
 
-app.use('/', express.static(__dirname + './public'));
+app.use('/', express.static(__dirname + '/public'));
 
 const routeEcosecador = require('./routes/ecosecador')(app);
 
