00332b643c03e6fa06962ed6a3cc2ad6974bc35b
CristianPalta1 <cristian.palta.c@uniautonoma.edu.co>
2019-09-30 20:08:54 -0500

    Commi errores BD

--- server/db/dbconnection.js
+++ server/db/dbconnection.js
@@ -5,7 +5,11 @@ module.exports = {
     //como es una promesa debe esperar hasta que se cumpla
     conectar : async (app) => {
         //asi se puede verificar si se hizo el proceso o no
-        await mongo.connect('mongodb://localhost:27017/DBecosecador',{ useNewUrlParser: true });
+        await mongo.connect('mongodb://localhost:27017/DBecosecador',{ useNewUrlParser: true }).then(() => {
+            console.log("Conecte a la BD mongo");
+        }).cacth((err) =>{
+            console.log("No me pude conectar",err);
+        });
         app.listen(3000, () => {
             console.log(`Ing. Cp i'm initialized on port 3000 `);
         });
